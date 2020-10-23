ba7edd9d36f56bfeb6d8df0e555dda608a84d095
Patryk <dev.droznyk@gmail.com>
2020-10-23 09:48:53 +0200

    Revert "Add more generators to Faker::Minecraft (#2162)"
    This reverts commit 4d3263cab1ff1cbf6d9d84c1e0fe49ba9a017a22.

--- test/faker/games/test_faker_minecraft.rb
+++ test/faker/games/test_faker_minecraft.rb
@@ -7,24 +7,8 @@ class TestFakerMinecraft < Test::Unit::TestCase
     @minecraft = Faker::Games::Minecraft
   end
 
-  def test_achievement
-    assert @minecraft.achievement.match(/\w+/)
-  end
-
-  def test_biome
-    assert @minecraft.biome.match(/\w+/)
-  end
-
   def test_block
     assert @minecraft.block.match(/\w+/)
-  end
-
-  def test_enchantment
-    assert @minecraft.enchantment.match(/\w+/)
-  end
-
-  def test_game_mode
-    assert @minecraft.game_mode.match(/\w+/)
   end
 
   def test_item
@@ -33,9 +17,5 @@ class TestFakerMinecraft < Test::Unit::TestCase
 
   def test_mob
     assert @minecraft.mob.match(/\w+/)
-  end
-
-  def test_status_effect
-    assert @minecraft.status_effect.match(/\w+/)
   end
 end
